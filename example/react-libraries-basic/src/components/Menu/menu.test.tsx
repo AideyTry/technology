31bcc2de99d441953f28620ee3fb041fec04ef96
jason19901028 <jason_dlb@sina.cn>
2020-12-02 22:38:34 +0800

    menu test

--- example/react-libraries-basic/src/components/Menu/menu.test.tsx
+++ example/react-libraries-basic/src/components/Menu/menu.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
 import Menu, { MenuProps } from './menu'
 import MenuItem from './menuItem'
 
@@ -15,7 +15,7 @@ const testVerProps: MenuProps = {
 const GenerateMenu = (props: MenuProps) => {
     return(<Menu {...props}>
         <MenuItem index={0}>active</MenuItem>
-        <MenuItem index={1}>disabled</MenuItem>
+        <MenuItem index={1} disabled>disabled</MenuItem>
         <MenuItem index={2}>menu item</MenuItem>
     </Menu>)
 }
@@ -32,11 +32,24 @@ describe('test Menu and MenuItem component', () => {
     it('should render correct Menu and MenuItem based on default props', () => {
         expect(menuElement).toBeInTheDocument()
         expect(menuElement).toHaveClass('turnip-menu test-class')
+        expect(menuElement.getElementsByTagName('li').length).toEqual(3)
+        expect(activeElement).toHaveClass('menu-item is-active')
+        expect(disabledElement).toHaveClass('menu-item is-disabled')
     })
     it('click items should change active and call the right callback', () => {
-
+        const thirdItem = wrapper.getByText('menu item')
+        fireEvent.click(thirdItem)
+        expect(thirdItem).toHaveClass('is-active')
+        expect(activeElement).not.toHaveClass('is-active')
+        expect(testProps.onSelect).toHaveBeenCalledWith(2)
+        fireEvent.click(disabledElement)
+        expect(disabledElement).not.toHaveClass('is-active')
+        expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
     })
     it('should render vertical mode when mode is set to vertical', () => {
-
+        cleanup()
+        wrapper = render(GenerateMenu(testVerProps))
+        menuElement = wrapper.getByTestId('test-menu')
+        expect(menuElement).toHaveClass('menu-vertical')
     })
 })
